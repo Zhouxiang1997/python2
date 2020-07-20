37176c5273c41fa0eca84bb2411c37f6a410adf1
ZX <54984343+Zhouxiang1997@users.noreply.github.com>
2020-07-20 12:22:33 +0800

    2020.7.20.12：22

--- Day05/00-Day05---ZX练习.py
+++ Day05/00-Day05---ZX练习.py
@@ -5,3 +5,39 @@ def fff(a):
         result = result * i
     print(result)
 fff(1)
+
+print('.............................---.........................................')
+myname='周翔'
+name='庄悦'
+def my():
+    # name='zx'
+    print('{} {}'.format(name,myname))
+    pass
+def test():
+    name='范冰冰'
+    print(myname)
+    pass
+my()
+test()
+
+print('.............................---.........................................')
+def change():
+    '''
+    修改全局变量
+    :return:
+    '''
+    global name
+    name='刘慧敏'
+    pass
+change()
+print(name)
+print('.............................---.........................................')
+a=1
+def fun(x):
+    print(id(x))
+    x=2
+    print(id(x))
+    pass
+print('a的地址{}'.format(id(a)))
+fun(a)
+print(a)
