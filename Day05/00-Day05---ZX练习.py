a258079f2794d1b58078cf4e7154a5d162c0ee00
ZX <54984343+Zhouxiang1997@users.noreply.github.com>
2020-07-23 22:24:16 +0800

    2020.7.23. 22：24

--- Day05/00-Day05---ZX练习.py
+++ Day05/00-Day05---ZX练习.py
@@ -99,3 +99,68 @@ def funtest(x, y): return x if x > y else y
 print(funtest(2, 4))
 ea = (lambda x, y: x if x > y else y)(3, 6)
 print(ea)
+
+
+print('.............................---.........................................')
+
+
+def jiecheng(n):
+    result = 1
+    for item in range(1, n + 1):
+        result *= item
+        pass
+    return result
+
+
+print('阶乘{}'.format(jiecheng(3)))
+
+
+print('.............................---.........................................')
+
+
+def digui(n):
+    '''
+    递归函数
+    :param n:
+    :return:
+    '''
+    if n == 1:
+        return 1
+    else:
+        return n * digui(n - 1)
+    pass
+
+
+print('阶乘{}'.format(digui(3)))
+print('.............................---.........................................')
+import os
+# def findFile(file_Path):
+#     listRs=os.listdir(file_Path)
+#     for fileitem in listRs:
+#         full_path=os.path.join(file_Path,fileitem)
+#         if os.path.isdir(full_path):
+#             findFile(full_path)
+#         else:
+#             print(fileitem)
+#             pass
+#         pass
+#     else:
+#         return
+#   findFile('F:\\Mari')
+import os #引入文件操作模块
+def findFile(file_Path):
+    listRs=os.listdir(file_Path) #得到该路径下所有文件夹
+    for fileItem in listRs:
+        full_path=os.path.join(file_Path,fileItem) #获取完整的文件路径
+        if os.path.isdir(full_path):#判断是否是文件夹
+            findFile(full_path) #如果是一个文件夹 再次去递归
+        else:
+            print(fileItem)
+            pass
+        pass
+    else:
+        return
+    pass
+
+# 调用搜索文件夹对象
+findFile('E:\\360Downloads')
